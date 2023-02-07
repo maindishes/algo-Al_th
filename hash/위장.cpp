29f9a123129b8507b74f722b772382a0e3f6efab
maindishes <manynolan1@gmail.com>
2023-02-07 15:17:26 +0900

    위장 수학적 문제 해결

--- hash/위장.cpp
+++ hash/위장.cpp
@@ -3,17 +3,20 @@
 #include <iostream>
 #include <map>
 // using namespace std;
-unsolved
+// aa b 일때
+// 입고 안 입을 경우 2가지 각각 a,b에 대해서 근데 둘다 안 입는 경우는 안되닌까 
+// (2+1)*(1+1) -1
 int solution(std::vector<std::vector<std::string> > clothes) {
-    int answer = 0;
+    int answer = 1;
     std::vector<int> i_list;
     std::map<std::string,int> i_map;
     for(int i =0; i<clothes.size(); ++i ) {
         i_map[clothes[i][1]] += 1;
     }
     std::map<std::string,int>::iterator it = i_map.begin();
-    for(std::map<std::string,int>::iterator it = i_map.begin(); it != i_map.end(); ++it) {
+    for(std::map<std::string,int>::iter4ator it = i_map.begin(); it != i_map.end(); ++it) {
+        answer *= (it->second +1);
         std::cout << it->second << std::endl;
     }
-    return answer;
+    return answer-1;
 }
